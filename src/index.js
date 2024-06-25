205d4f93165898c324a0921de92745bc5860d692
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-06-25 20:20:59 +0400

    modified src/inde.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-export const hi = () => console.log("Hello, Hexlet!");
-export const last = () => console.log(_.last(["one", "two"]));
+export const hi = () => console.log('Hello, Hexlet!');
+export const last = () => console.log(_.last(['one', 'two']));
